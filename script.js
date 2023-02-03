06572e8ba03161ffb966e10380156b323c6070fd
SharpeimQ <SharpHaiyan@gmail.com>
2023-02-03 12:20:25 -0500

    Build sketchpad creation algorithm

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const container = document.querySelector('.container');
-numBoxes = 256;
+vertical = 16;
+horizontal = 16;
 
-for (i = 0; i < numBoxes; i++) {
-    let boxes = document.createElement('div');
-    boxes.classList.add('boxStyle');
-    container.appendChild(boxes);
+for (i = 0; i < vertical; i++) {
+    let row = document.createElement('div');
+    row.classList.add('rowStyle');
+    container.appendChild(row);
+    for (j = 0; j < horizontal; j++) {
+        let box = document.createElement('div');
+        box.classList.add('boxStyle');
+        row.appendChild(box);
+    }
 }
+
+//boxStyle.addEventListener('mouseenter', () => {
+//    boxes.classList.add('hover');
+//})
