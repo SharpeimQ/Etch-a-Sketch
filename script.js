2c9cf93559a040e470296d68e1c5d08d97241c9d
SharpeimQ <SharpHaiyan@gmail.com>
2023-02-04 20:52:26 -0500

    Add change dimensions feature

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 const container = document.querySelector('.container');
-vertical = 16;
-horizontal = 16;
+const button = document.querySelector('.btn-dimensions');
+let vertical = 16;
+let horizontal = 16;
 
 //Container creation 
-for (i = 0; i < vertical; i++) {
-    let row = document.createElement('div');
-    row.classList.add('rowStyle');
-    container.appendChild(row);
-    for (j = 0; j < horizontal; j++) {
-        let box = document.createElement('div');
-        box.classList.add('boxStyle');
-        row.appendChild(box);
+const createGrid = (vertical, horizontal) => {
+    for (let i = 0; i < vertical; i++) {
+        let row = document.createElement('div');
+        row.classList.add('rowStyle');
+        container.appendChild(row);
+        for (let j = 0; j < horizontal; j++) {
+            let box = document.createElement('div');
+            box.classList.add('boxStyle');
+            row.appendChild(box);
 
-        //Hover Effects
-        box.addEventListener('mouseenter', () => {
-            box.classList.add('hover');
-        })
-        box.addEventListener('mouseleave', () => {
-            box.classList.remove('hover');
-        })
+            //Hover Effects
+            box.addEventListener('mouseenter', () => {
+                box.classList.add('hover');
+            })
+            box.addEventListener('mouseleave', () => {
+                box.classList.remove('hover');
+            })
+        }
     }
 }
+
+createGrid(vertical, horizontal);
+
+button.addEventListener('click', () => {
+    container.replaceChildren();
+
+    do {
+    let temp = prompt('Enter the height for the Sketchpad', 16);
+    vertical = +temp;
+    } while ((vertical <= 0) || (vertical > 100));
+
+    do {
+    let temp2 = prompt('Enter the width for the Sketchpad', 16);
+    horizontal = +temp2;
+    } while ((horizontal <= 0) || (horizontal > 100));
+    
+    createGrid(vertical, horizontal);
+})
+
